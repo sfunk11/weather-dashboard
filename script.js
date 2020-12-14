cab3aaa3197b8e2b58e6274a2baddaa12e1d183e
Lady Teldra <73320959+sfunk11@users.noreply.github.com>
2020-12-14 09:08:10 -0500

    fixed icon code

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ dayjs.extend(window.dayjs_plugin_localizedFormat);
        
 // for initial Weather data call
 var APIKey = "a71ffd832a9960499b2f56bba5c397e5";
-var searchCity = "Atlanta"
+var searchCity = "Atlanta";
 
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + searchCity +"&appid=" + APIKey;
 
 
 function getWeather(queryURL){
@@ -14,14 +13,17 @@ $.ajax({
     method: "GET"
   }).then(function(response) {
      coords = response.coord;
+     console.log(response);
      console.log(response.coord);
-     cityNameEl = $("<h1>").text(searchCity + " ("+ today + ")" +response.weather.icon);
+     imgURL = "http://openweathermap.org/img/wn/"+ response.weather[0].icon + "@2x.png";
+     currentIconEl = $("<img>").attr("src", imgURL);
+     cityNameEl = $("<h1>").html(searchCity + " ("+ today + ")");
      tempEl = $("<p>").text("Temperature: "+ response.main.temp + `\xB0 F`);
       humidityEl = $("<p>").text("Humidity: " + response.main.humidity + `%`);
       windspeedEl = $("<p>").text("Wind Speed: "+ response.wind.speed + "MPH");
       uvIndex = $("<p>").html("UV Index: <span class = 'uv'></span>");
       // $(".uv").html HOW TO FIND UV INDEX?
-     $("#weather-display").append(cityNameEl, tempEl, humidityEl, windspeedEl, uvIndex);
+     $("#weather-display").append(cityNameEl, currentIconEl, tempEl, humidityEl, windspeedEl, uvIndex);
 
 // for forecast call and population
       lat = coords.lat;
@@ -33,14 +35,14 @@ $.ajax({
          url: forecastURL,
          method: "GET"
        }).then(function(response) {
-           console.log(response);
            $(".uv").text(response.current.uvi);
            var forecast = response.daily;
 
            for(var i=1; i < 6; i++){
             date = new Date(forecast[i].dt * 1000);
+            imgURL = "http://openweathermap.org/img/wn/"+ forecast[i].weather[0].icon + "@2x.png";
             dateEl = $("<h5>").text(dayjs(date).format("LL")).attr("class", "card-title");
-            iconEl = $("<p>").text(forecast[i].weather.icon);
+            iconEl = $("<img>").attr("src", imgURL);
             forecastTempEl = $("<p>").text("Temp: "+ forecast[i].temp.day + `\xB0 F`);
             humidityEl = $("<p>").text("Humidity: " + forecast[i].humidity + `%`);
             forecastid = "#forecast-" + (i);
